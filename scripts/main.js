b8ed6abd7556a796335b98dc1e9fdb184a5f4a4f
Yash9028 <dyash9028@gmail.com>
2024-09-16 14:34:10 +0000

    done

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+const resetBtn = document.querySelector(".btn-reset");
+const gameGrid = document.querySelector(".game-grid");
+resetBtn.addEventListener("click", function (){
+  console.log("hii");
+  resetBtn.addEventListener()
+  gameGrid.style.backgroundColor = "yellow";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
