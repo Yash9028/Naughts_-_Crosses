2cc28cb86fe77d6055b777bbad74fb5376018293
Yash9028 <dyash9028@gmail.com>
2024-09-23 14:29:55 +0000

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,32 @@
+const headingEl = document.querySelector(".heading")
+const currentPlayerEl = document.querySelector(".current-player")
+const boxes = document.querySelectorAll(".box");
 const resetBtn = document.querySelector(".btn-reset");
-const gameGrid = document.querySelector(".game-grid");
-resetBtn.addEventListener("click", function (){
-  console.log("hii");
-  resetBtn.addEventListener()
-  gameGrid.style.backgroundColor = "yellow";
+
+
+const player=["O","X"];
+let currPlayer = getPlayer();
+currentPlayerEl.textContent = currPlayer;
+
+
+function getPlayer(){
+  const rn = Math.floor(Math.random()*2);
+  return players[rn];
+}
+function swapPlayer(){
+  currPlayer = currPlayer ==="O"?"X":"O";
+}
+function handleClick(input){
+  input.textContent = currPlayer;
+  swapPlayer();
+  currentPlayerEl.textContent = currPlayer;
+}
+
+//only the first one
+boxes.forEach(function(box){
+  box.addEventListener("click",function(){
+
+    handleClick(box);
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
